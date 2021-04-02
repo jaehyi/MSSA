3b5e11ee05dbe8a16d773029b8369f7b78c36dd8
Jae Yi <jae.h.yi@outlook.com>
2021-04-01 21:25:09 -0400

    Adding PE11

---
+++ C-Sharp/VectorDistanceCalculation/PE11VectorDistanceCalculation/Point3D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE11VectorDistanceCalculation
+{
+    class Point3D : IEquatable<Point3D>
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public Point3D()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+
+        public Point3D(Random random, int min = 1, int max = 1001)
+        {
+            X = random.Next(min, max);
+            Y = random.Next(min, max);
+            Z = random.Next(min, max);
+        }
+
+        public double CalculateDistanceTo(Point3D point)
+        {
+            return Math.Sqrt(Math.Pow((point.X - X), 2) + Math.Pow((point.Y - Y), 2) + Math.Pow((point.Z - Z), 2));
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}, Z: {Z}";
+        }
+
+        public bool Equals(Point3D other)
+        {
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+    }
+}
